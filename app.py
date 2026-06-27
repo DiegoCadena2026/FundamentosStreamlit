1973d67fe8c0328b7808a78b303af799d4e8218f
DiegoCadena2026 <dcadena@emagic.fin.ec>
2026-06-27 08:59:13 -0500

    Add loan calculation inputs and result computation

--- app.py
+++ app.py
@@ -6,4 +6,12 @@ st.sidebar.image("LogoUHE.png")
 
 st.sidebar.title("Parámetros")
 
+monto = st.number_input("Ingrese el monto de su préstamo")
+
+interes = st.number_input("Ingrese el interes", value = 0.05)
+
+tiempo_meses = st.number_input("Ingrese el tiempo en meses")
+
+resultado = monto*interes+(tiempo_meses/12)
+
 
